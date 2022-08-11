314f9b8ffe717e346f18fe01f1bc9e2dc6aa3ce5
erikalves12 <97927458+erikalves12@users.noreply.github.com>
2022-08-11 17:31:30 -0300

    atualização

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,20 +1,17 @@
 import { useState, useEffect } from "react";
 
-import PaletaListaItem from "components/PaletaListaItem/PaletaListaItem"
+import PaletaListaItem from "components/PaletaListaItem/PaletaListaItem";
 import { PaletaService } from "services/PaletaService";
 
 import "./PaletaLista.css";
 import PaletaDetalhesModal from "components/PaletaDetalhesModal/PaletaDetalhesModal";
 
 function PaletaLista({ paletaCriada }) {
-
   const [paletas, setPaletas] = useState([]);
 
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
   const [paletaModal, setPaletaModal] = useState(false);
-
-  
 
   const adicionarItem = (paletaIndex) => {
     const paleta = {
@@ -24,51 +21,58 @@ function PaletaLista({ paletaCriada }) {
   };
 
   const removerItem = (paletaIndex) => {
-    const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1 }
-    setPaletaSelecionada({...paletaSelecionada, ...paleta});
-}
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) - 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
 
-const getLista = async () => {
-  const response = await PaletaService.getLista();
-  setPaletas(response);
-};
+  const getLista = async () => {
+    const response = await PaletaService.getLista();
+    setPaletas(response);
+  };
 
-const getPaletaById = async (paletaId) => {
-  const response = await PaletaService.getById(paletaId);
-  setPaletaModal(response);
-};
+  const getPaletaById = async (paletaId) => {
+    const response = await PaletaService.getById(paletaId);
+    setPaletaModal(response);
+  };
 
-const adicionaPaletaNaLista = (paleta) => {
-  const lista = [...paletas, paleta];
-  setPaletas(lista);
-};
+  
+  useEffect(() => {
+    getLista();
+  }, []);
 
-useEffect(() => {
-  if (paletaCriada) adicionaPaletaNaLista(paletaCriada);
-}, [paletaCriada]);
-useEffect(() => {
-  getLista();
-}, []);
+  const adicionaPaletaNaLista = (paleta) => {
+    const lista = [...paletas, paleta];
+    setPaletas(lista);
+  };
+
+  useEffect(() => {
+    if (paletaCriada) adicionaPaletaNaLista(paletaCriada); 
+  }, [paletaCriada]);
+
 
   return (
     <div className="PaletaLista">
-	{
-  paletas.map((paleta, index) => (
-    <PaletaListaItem
-      key={`PaletaListaItem-${index}`}
-      paleta={paleta}
-      quantidadeSelecionada={paletaSelecionada[index]}
-      index={index}
-      onAdd={(index) => adicionarItem(index)}
-      onRemove={(index) => removerItem(index)}
-      clickItem={(paletaId) => getPaletaById(paletaId)}
-    />
-  ))
-}
+      {paletas.map((paleta, index) => (
+        <PaletaListaItem
+          key={`PaletaListaItem-${index}`}
+          paleta={paleta}
+          quantidadeSelecionada={paletaSelecionada[index]}
+          index={index}
+          onAdd={(index) => adicionarItem(index)}
+          onRemove={(index) => removerItem(index)}
+          clickItem={(paletaId) => getPaletaById(paletaId)}
+        />
+      ))}
 
-{paletaModal && <PaletaDetalhesModal paleta={paletaModal} closeModal={() => setPaletaModal(false)} />}
-
-</div>
+      {paletaModal && (
+        <PaletaDetalhesModal
+          paleta={paletaModal}
+          closeModal={() => setPaletaModal(false)}
+        />
+      )}
+    </div>
   );
 }
 
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,26 +1,27 @@
 import "./Home.css";
 import PaletaLista from "components/PaletaLista/PaletaLista.jsx";
-import Navbar from "components/Navbar/Navbar"
+import Navbar from "components/Navbar/Navbar";
 import AdicionaPaletaModal from "components/AdicionaPaletaModal/AdicionaPaletaModal";
 
 import { useState } from "react";
 
 function Home() {
-  const [canShowAdicionaPaletaModal, setCanShowAdicionaPaletaModal] = useState(false);
+  const [canShowAdicionaPaletaModal, setCanShowAdicionaPaletaModal] =
+    useState(false);
   const [paletaParaAdicionar, setPaletaParaAdicionar] = useState();
+
   return (
     <div className="Home">
       <Navbar createPaleta={() => setCanShowAdicionaPaletaModal(true)} />
 
       <div className="Home__container">
-      <PaletaLista paletaCriada={paletaParaAdicionar} />
-                {
-                    canShowAdicionaPaletaModal && (
-                    <AdicionaPaletaModal
-                        closeModal={() => setCanShowAdicionaPaletaModal(false)}
-                        onCreatePaleta={(paleta) => setPaletaParaAdicionar(paleta)} />
-                    )
-                }
+        <PaletaLista paletaCriada={paletaParaAdicionar} />
+        {canShowAdicionaPaletaModal && (
+          <AdicionaPaletaModal
+            closeModal={() => setCanShowAdicionaPaletaModal(false)}
+            onCreatePaleta={(paleta) => setPaletaParaAdicionar(paleta)}
+          />
+        )}
       </div>
     </div>
   );
